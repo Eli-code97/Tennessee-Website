24144accab4eaf3f4b4887ea803ab1a345d0558c
Eli-code97 <Emuhammadawesome@gmail.com>
2024-09-19 14:41:51 -0500

    Add files via upload

--- Tennesscript.js
+++ Tennesscript.js
@@ -1,14 +1,16 @@
 
 var email = document.getElementById('email')
 var inValid = document.getElementById('inValid')
+var emailv = document.getElementById('emailv')
+var Question = document.getElementById('questions')
 
 
 
 function validateEmail(){
 
-    if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+    if(!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)  & email !=("emailv"))  {
         console.log("invalid email address")
-        inValid.innerHTML ="invalid email address";
+        inValid.innerHTML ="invalid email address or email does not match verification";
         return false;
     }
     else {
@@ -16,7 +18,17 @@ function validateEmail(){
         inValid.innerHTML ="  ";
         return true;
     }
+
     
+
+}
+
+
+
+
+function questlog(){
+
+    console.log("question:", Question);
 }
 
 
